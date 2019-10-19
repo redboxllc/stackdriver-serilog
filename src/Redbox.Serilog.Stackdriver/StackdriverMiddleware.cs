baedc23789f82c7fc77a1855d877361314958353
Lin Meyer <lin.meyer@redbox.com>
2019-10-18 21:06:30 -0500

    Added remoteIp

--- src/Redbox.Serilog.Stackdriver/StackdriverMiddleware.cs
+++ src/Redbox.Serilog.Stackdriver/StackdriverMiddleware.cs
@@ -5,9 +5,16 @@ using Serilog;
 
 namespace Redbox.Serilog.Stackdriver
 {
+    /// <summary>
+    /// Middleware that adds additional Stackdriver LogEvent properties not found in ASP's default logging
+    /// </summary>
     public class StackdriverLoggingMiddleware
     {
-        public static string StackdriverKeyPrefix = "Stackdriver_";
+        /// <summary>
+        /// Prefix used for the key in log properties to avoid collisions with other logging frameworks or ASP
+        /// </summary>
+        /// <returns></returns>
+        public static string StackdriverKeyPrefix = $"{nameof(StackdriverLoggingMiddleware)}_";
         private readonly RequestDelegate _next;
         private readonly IDiagnosticContext _diagnosticContext;
 
@@ -19,16 +26,18 @@ namespace Redbox.Serilog.Stackdriver
 
         public async Task Invoke(HttpContext httpContext)
         {
-            var httpConnection = httpContext?.Features?.Get<IHttpConnectionFeature>();
             // Request 
             HandleUserAgent(httpContext?.Request);
+            HandleReferer(httpContext?.Request);
+
+            var httpConnection = httpContext?.Features?.Get<IHttpConnectionFeature>();
             HandleServerIp(httpConnection);
-            
+            HandleRemoteIp(httpContext?.Request, httpConnection);
+
             // Trigger response/other middlewares
             await _next.Invoke(httpContext);
             
             // Response
-            
         }
 
         /// <summary>
@@ -51,6 +60,15 @@ namespace Redbox.Serilog.Stackdriver
             }
         }
 
+        private void HandleReferer(HttpRequest request)
+        {
+            var referer = request?.Headers["Referer"].ToString();
+            if(!string.IsNullOrWhiteSpace(referer))
+            {
+                AddToLog("referer", referer);
+            }
+        }
+
         private void HandleServerIp(IHttpConnectionFeature httpConnection)
         {
             if(httpConnection == null) return;
@@ -58,6 +76,27 @@ namespace Redbox.Serilog.Stackdriver
             if(!string.IsNullOrWhiteSpace(localIpAddress))
             {
                 AddToLog("serverIp", localIpAddress);
+            }
+        }
+
+        private void HandleRemoteIp(HttpRequest request, IHttpConnectionFeature httpConnection)
+        {
+            if(httpConnection == null) return;
+
+            // Check for LB/proxy forwarded header first
+            // If this is set Remote IP is likely incorrect
+            var forwardedIp = request?.Headers["X-Forwarded-For"].ToString();
+            var key = "remoteIp";
+            if(!string.IsNullOrWhiteSpace(forwardedIp))
+            {
+                AddToLog(key, forwardedIp);
+                return;
+            }
+
+            var remoteIp = httpConnection?.RemoteIpAddress?.ToString();
+            if(!string.IsNullOrWhiteSpace(remoteIp))
+            {
+                AddToLog(key, remoteIp);
             }
         }
     }
